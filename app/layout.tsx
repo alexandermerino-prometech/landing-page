6e943404a3f9650247b5bf7ad564e49fc37352a4
Alexander Merino <alexander.merino@prometech.com.pe>
2026-07-22 19:34:58 -0500

    agregar vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata,Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import Schema from "./components/Schema";
@@ -130,6 +132,9 @@ export default function RootLayout({
         <Schema />
         <Navbar />
         <main className="flex-1">{children}</main>
+
+        <Analytics />
+         <SpeedInsights />
       </body>
     </html>
   );
