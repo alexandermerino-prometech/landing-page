4a98b142675b03713a51072e66f830c4d0fbf92c
Alexander Merino <alexander.merino@prometech.com.pe>
2026-07-22 22:00:24 -0500

    arreglando las fotos 4

--- app/components/EquipoExperiencia.tsx
+++ app/components/EquipoExperiencia.tsx
@@ -68,7 +68,7 @@ export default function EquipoExperiencia() {
                   src={persona.foto}
                   alt={persona.nombre}
                   fill
-                  className="object-cover object-top"
+                  className="object-cover object-top scale-85"
                 />
               </div>
 
