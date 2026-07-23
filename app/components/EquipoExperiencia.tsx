4361cc6ff8650108ef731450f30c07d855ccaf5d
Alexander Merino <alexander.merino@prometech.com.pe>
2026-07-22 22:49:17 -0500

    moviendo la foto para que este centrado

--- app/components/EquipoExperiencia.tsx
+++ app/components/EquipoExperiencia.tsx
@@ -68,7 +68,7 @@ export default function EquipoExperiencia() {
                   src={persona.foto}
                   alt={persona.nombre}
                   fill
-                  className="object-cover object-[center_15%] scale-150"
+                  className="object-cover object-[center_15%] scale-200"
                 />
               </div>
 
